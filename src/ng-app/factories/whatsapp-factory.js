c9b4bc6391d5359537605a3c81c6f6e47ea4d546
jack lam <jacklam718@gmail.com>
2015-05-12 04:29:19 +0800

    added whatsapp-factory to provide some basic features

---
+++ src/ng-app/factories/whatsapp-factory.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var WhatsAppFactory = function($rootScope, $q) {
+  var self = this;
+  var whatsapi = require('whatsapi');
+
+  return {
+    createAdapter: function(argument) {
+      self.wa = whatsapi(argument);
+    },
+
+    login: function(logged) {
+      // logged is callback function
+      if (logged === undefined) {
+        logged = function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      }
+
+      self.wa.connect(function(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        self.wa.login(logged);
+      })
+    },
+
+    requestGroupsList: function() {
+      wa.requestGroupsList(function(err, groups) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      })
+    },
+
+    requestLastSeen: function(to, func) {
+      if (func === undefined) {
+        func = function(err, lastSeen) {
+          if (err && err.code == 405) {
+            console.log('The user has denied last seen time');
+            return;
+          }
+          console.log(lastSeen.date);
+        }
+      }
+
+      self.wa.requestLastSeen(to, func);
+    },
+
+    isLoggedIn: function() {
+      return self.wa.isLoggedIn();
+    }
+  }
+};
+
+WhatsAppDesktop.factory("WhatsAppFactory", WhatsAppFactory);
