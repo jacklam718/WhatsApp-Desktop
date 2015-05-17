cd9182ab4016d1977cf8d9151690945f431ff625
jack lam <jacklam718@gmail.com>
2015-05-17 23:39:37 +0800

    whatsapp-service.js changed to using async

--- src/ng-app/services/whatsapp-service.js
+++ src/ng-app/services/whatsapp-service.js
@@ -1,77 +1,92 @@
 "use strict";
 
 var WhatsAppService = function($rootScope, $q) {
-  var whatsapi = require('whatsapi');
+  var whatsApi = require('whatsapi');
 
   return {
     createAdapter: function(argument) {
       var self = this;
-      self.wa = whatsapi.createAdapter(argument);
-      console.log("createAdapter: ", self.wa);
+      self.whatsApi = whatsApi.createAdapter(argument);
+      console.log("createAdapter: ", self.whatsApi);
     },
 
     login: function(logged) {
       var self = this;
+      var deferred = $q.defer();
 
       // logged is callback function
       if (logged === undefined) {
         logged = function (err) {
           if (err) {
             console.error(err);
+            deferred.reject(err);
             return;
           }
           console.log("Logged in to WA server");
-          self.wa.sendIsOnline();
+          self.whatsApi.sendIsOnline();
+          deferred.resolve(true);
         }
       }
 
-      self.wa.connect(function(err) {
+      self.whatsApi.connect(function(err) {
         if (err) {
           console.error(err);
+          deferred.reject(err);
           return;
         }
 
-        self.wa.login(logged);
+        self.whatsApi.login(logged);
+        console.log("login");
       });
-      console.log("login");
+
+      return deferred.promise;
     },
 
     requestGroupsList: function() {
       var self = this;
+      var deferred = $q.defer();
 
       console.log("requestGroupsList");
-      self.wa.requestGroupsList(function(err, groups) {
+      self.whatsApi.requestGroupsList(function(err, groups) {
         console.log(groups);
         if (err) {
           console.error(err);
+          deferred.reject(err);
           return;
         }
 
-      })
+        deferred.resolve(groups);
+      });
+
+      return deferred.promise;
     },
 
     requestLastSeen: function(to, func) {
       var self = this;
+      var deferred = $q.defer();
 
       if (func === undefined) {
         func = function(err, lastSeen) {
           if (err && err.code == 405) {
             console.log('The user has denied last seen time');
+            deferred.reject(err);
             return;
           }
+
           console.log(lastSeen.date);
+          deferred.resolve(lastSeen);
         }
       }
 
-      self.wa.requestLastSeen(to, func);
+      self.whatsApi.requestLastSeen(to, func);
       console.log("requestLastSeen");
+
+      return deferred.promise;
     },
 
     isLoggedIn: function() {
-      var self = this;
-
-      console.log(self.wa);
-      return self.wa;
+      console.log("isLoggedIn");
+      return this.whatsApi.isLoggedIn();
     }
   }
 };
