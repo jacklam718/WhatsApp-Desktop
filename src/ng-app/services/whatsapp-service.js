76f5d8bda1c99000971ca5cc7fc77a5b981ac125
jack lam <jacklam718@gmail.com>
2015-05-18 03:46:21 +0800

    added whats app event listen functions on whats-service.js

--- src/ng-app/services/whatsapp-service.js
+++ src/ng-app/services/whatsapp-service.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 var WhatsAppService = function($rootScope, $q) {
   var whatsApi = require('whatsapi');
@@ -7,12 +7,15 @@ var WhatsAppService = function($rootScope, $q) {
     createAdapter: function(argument) {
       var self = this;
       self.whatsApi = whatsApi.createAdapter(argument);
+      wa = self.whatsApi
       console.log("createAdapter: ", self.whatsApi);
     },
 
-    login: function(logged) {
+    login: function(logged, whatsAppInfo) {
       var self = this;
       var deferred = $q.defer();
+
+      self.createAdapter(whatsAppInfo);
 
       // logged is callback function
       if (logged === undefined) {
@@ -86,7 +89,79 @@ var WhatsAppService = function($rootScope, $q) {
 
     isLoggedIn: function() {
       console.log("isLoggedIn");
-      return this.whatsApi.isLoggedIn();
+      return this.whatsApi === undefined ? false : this.whatsApi.isLoggedIn();
+    },
+
+    receivedMessage: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(message) {
+          console.log(message);
+        }
+      }
+
+      self.whatsApi.on("receivedMessage", handler);
+    },
+
+    receivedLocation: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(location) {
+          console.log(location);
+        }
+      }
+
+      self.whatsApi.on("receivedLocation", handler);
+    },
+
+    receivedImage: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(image) {
+          console.log(image);
+        }
+      }
+
+      self.whatsApi.on("receivedImage", handler);
+    },
+
+    receivedVideo: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(video) {
+          console.log(video);
+        }
+      }
+
+      self.whatsApi.on("receivedVideo", handler);
+    },
+
+    receivedAudio: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(audio) {
+          console.log(audio);
+        }
+      }
+
+      self.whatsApi.on("receivedAudio", handler);
+    },
+
+    receivedVcard: function (handler) {
+      var self = this;
+
+      if (handler === undefined) {
+        handler = function(vcard) {
+          console.log(vcard);
+        }
+      }
+
+      self.whatsApi.on("receivedVcard", handler);
     }
   }
 };
